246fe70729210b27048b8c7a8de661a8e012b306
ringleschavez <r.ingles.chavez@gmail.com>
2021-07-03 22:03:08 +0200

    01_01_ from local VM

--- multi_node_shared_data/01_01_mmap_shared_data.py
+++ multi_node_shared_data/01_01_mmap_shared_data.py
@@ -9,13 +9,18 @@ from mpi4py import MPI
 SHARED_DATA_PATH = './'
 SHARED_DATA_FILENAME = 'shared_data.mmap'
 
-SHARED_DATA_SIZE = 8 
+SHARED_DATA_SIZE = 8
 
-class Command(IntFlag):
-    IDLE = 0 
-    START = 1
-    STOP = 2
-    DO_THE_NEEDFUL = 3
+MMAP_FILENAME_KEY = 'mmap_filename'
+
+mmap_path_filename = None
+
+
+class ProcessCommand(IntFlag):
+    IDLE = 65
+    START = 66
+    STOP = 67
+    DO_THE_NEEDFUL = 68
 
 
 mpi_cw_size = MPI.COMM_WORLD.Get_size()
@@ -33,23 +38,22 @@ if mpi_cw_rank == 0:
         file_object.write(bytes(SHARED_DATA_SIZE))
 
     with open(mmap_path_filename, 'r+', encoding='utf8') as file_object:
-        with mmap.mmap(file_object.fileno(), 
+        with mmap.mmap(file_object.fileno(),
                        length=0,
                        access=mmap.ACCESS_WRITE,
                        offset=0
-                    ) as mmap_object:
+                       ) as mmap_object:
             # just checking the content
             assert bytes(SHARED_DATA_SIZE) == mmap_object[:SHARED_DATA_SIZE]
 
-            data_to_be_broadcasted = {'mmap_filename': mmap_path_filename}
+            data_to_be_broadcasted = {MMAP_FILENAME_KEY: mmap_path_filename}
 
-            # initializating the mmap file content
-            for i in range(mpi_cw_size):
-                
+            # initializing the mmap file content
+            for i_rank in range(mpi_cw_size):
+                mmap_object[i_rank] = ProcessCommand.IDLE
 else:
     data_to_be_broadcasted = None  # not main rank
 
-    
 # all processes wait for the mmap file creation
 broadcasted_data = mpi_comm.bcast(data_to_be_broadcasted, root=0)
 
@@ -59,5 +63,35 @@ if mpi_cw_rank > 0:
                                               mpi_cw_size,
                                               broadcasted_data))
 
+#  Sending commands to the processes
+if mpi_cw_rank == 0:
+    with open(mmap_path_filename, 'r+', encoding='utf8') as file_object:
+        with mmap.mmap(file_object.fileno(),
+                       length=0,
+                       access=mmap.ACCESS_WRITE,
+                       offset=0
+                       ) as mmap_object:
 
-if 
+            for i_rank in range(mpi_cw_size):
+                mmap_object[i_rank] = ProcessCommand.STOP
+
+if mpi_cw_rank > 0:
+    mmap_path_filename = broadcasted_data[MMAP_FILENAME_KEY]
+    with open(mmap_path_filename, 'r+', encoding='utf8') as file_object:
+        with mmap.mmap(file_object.fileno(),
+                       length=0,
+                       access=mmap.ACCESS_READ,
+                       offset=0
+                       ) as mmap_object:
+
+            while True:
+                current_command = mmap_object[mpi_cw_rank]
+                if current_command == ProcessCommand.STOP:
+                    print('{}(rank={}/{}) stopped'.format(mpi_proc_name, mpi_cw_rank, mpi_cw_size))
+                    break
+
+                #  relinquishing CPU turn
+                os.sched_yield()
+
+broadcasted_data = mpi_comm.bcast(data_to_be_broadcasted, root=0)
+print('{}(rank={}/{}) ended'.format(mpi_proc_name, mpi_cw_rank, mpi_cw_size))
--- multi_node_shared_data/README.md
+++ multi_node_shared_data/README.md
@@ -1,11 +1,11 @@
-Buffer-like communication methods (upper-case):
+### Buffer-like communication methods (upper-case):
     Send(), Recv(), Bcast(), Scatter(), Gather().
 
-Python objects communication methods (lower-case):
+### Python objects communication methods (lower-case):
     send(), recv(), bcast(), scatter(), gather().
 
 
-MPI info sources:
+### MPI info sources:
 
 https://research.computing.yale.edu/sites/default/files/files/mpi4py.pdf
 
@@ -15,5 +15,5 @@ https://info.gwdg.de/wiki/doku.php?id=wiki:hpc:mpi4py
 
 https://courses.cs.ut.ee/MTAT.08.020/2019_spring/uploads/Main/MPI_and_mpi4py.pdf
 
-mmap info sources:
+### mmap info sources:
 https://realpython.com/python-mmap/
